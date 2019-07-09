e86b872c578c3d19bfbe5ec4ac219e55839a752a
tjbrailey <tbrailey@ucsd.edu>
2019-07-09 11:15:56 -0700

    write.csv

--- code/missile_test_code.R
+++ code/missile_test_code.R
@@ -100,6 +100,6 @@ missile_dat_final <- join
 rm(join)
 
 # Save joined data
-save(missile_dat_final, file = paste0(here::here(), '/data/missile_dat_final.csv'))
+write.csv(missile_dat_final, file = paste0(here::here(), '/data/missile_dat_final.csv'))
 
 visdat::vis_dat(missile_dat_final)
